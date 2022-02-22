e45feaf85557d9ba84eb2eec108f6b543a6d0ca2
ptiry02 <97674049+ptiry02@users.noreply.github.com>
2022-02-22 01:22:28 +0100

    finaly a decent menu

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -5,23 +5,23 @@ import profilePic from "../assets/images/yo_en_moto.jpg";
 class MainCard extends Component {
   render() {
     return (
-      <div class="card" id="inicio">
+      <div className="card" id="inicio">
         <div id="photo">
           <img src={profilePic} alt="Foto perfil" id="profilePic" />
         </div>
-        <div class="box">
-          <h1 class="text-center pt-3">PAUL TIRY</h1>
-          <h4 class="text-center">Mecánico y programador web</h4>
+        <div className="box">
+          <h1 className="text-center pt-3">PAUL TIRY</h1>
+          <h4 className="text-center">Mecánico y programador web</h4>
         </div>
-        <div class="info">
-          <p class="text-center">
-            <i class="fas fa-envelope"> tirypaul@gmail.com</i>
-            <i class="fas fa-phone-square"> +34 679 704 916</i>
+        <div className="info">
+          <p className="text-center">
+            <i className="fas fa-envelope"> tirypaul@gmail.com</i>
+            <i className="fas fa-phone-square"> +34 679 704 916</i>
           </p>
         </div>
-        <div class="icons text-center mt-3">
-          <i class="fab fa-linkedin fa-lg"></i>
-          <i class="fab fa-facebook-square fa-lg"></i>
+        <div className="icons text-center mt-3">
+          <i className="fab fa-linkedin fa-lg"></i>
+          <i className="fab fa-facebook-square fa-lg"></i>
         </div>
       </div>
     );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,67 +2,78 @@ import React, { Component } from "react";
 import "../assets/css/Menu.css";
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: true,
+    };
+
+    this.handleChange = (event) => {
+      const target = event.target;
+      const value = target.id;
+
+      this.setState({
+        selected: !this.state.selected,
+      });
+      console.log(value);
+    };
+  }
+
   render() {
     return (
       <div
-        class="btn-group-vertical"
-        role="group"
-        aria-label="Basic radio toggle button group"
+        className="btn-group-vertical"
         id="campos"
+        onChange={this.handleChange}
       >
-        <input
-          type="radio"
-          class="btn-check"
-          name="btnradio"
-          id="btnradio1"
-          autocomplete="off"
-          checked
-        />
-        <label class="btn btn-outline-primary" for="btnradio1">
+        <label className="btn btn-outline-primary" id="btnradio1">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio1"
+            defaultChecked
+          />
           inicio
         </label>
 
-        <input
-          type="radio"
-          class="btn-check"
-          name="btnradio"
-          id="btnradio2"
-          autocomplete="off"
-        />
-        <label class="btn btn-outline-primary" for="btnradio2">
+        <label className="btn btn-outline-primary" id="btnradio2">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio2"
+          />
           experiencia
         </label>
 
-        <input
-          type="radio"
-          class="btn-check"
-          name="btnradio"
-          id="btnradio3"
-          autocomplete="off"
-        />
-        <label class="btn btn-outline-primary" for="btnradio3">
+        <label className="btn btn-outline-primary" id="btnradio3">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio3"
+          />
           formación
         </label>
 
-        <input
-          type="radio"
-          class="btn-check"
-          name="btnradio"
-          id="btnradio4"
-          autocomplete="off"
-        />
-        <label class="btn btn-outline-primary" for="btnradio4">
+        <label className="btn btn-outline-primary" id="btnradio4">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio4"
+          />
           habilidades
         </label>
 
-        <input
-          type="radio"
-          class="btn-check"
-          name="btnradio"
-          id="btnradio5"
-          autocomplete="off"
-        />
-        <label class="btn btn-outline-primary" for="btnradio5">
+        <label className="btn btn-outline-primary" id="btnradio5">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio5"
+          />
           contacto
         </label>
       </div>
