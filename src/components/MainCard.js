c523b0deffe09780428c8036d9f8e08a7e880c04
ptiry02 <97674049+ptiry02@users.noreply.github.com>
2022-02-25 20:59:05 +0100

    first commit to deploy with vercel

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "../assets/css/MainCard.css";
 import profilePic from "../assets/images/yo_en_moto.jpg";
 
@@ -10,16 +11,14 @@ class MainCard extends Component {
           <img src={profilePic} alt="Foto perfil" id="profilePic" />
         </div>
         <div className="box">
-          <h1 className="text-center pt-3">PAUL TIRY</h1>
-          <h4 className="text-center">Mecánico y programador web</h4>
+          <h1>PAUL TIRY</h1>
+          <h4>Mecánico y programador web</h4>
         </div>
         <div className="info">
-          <p className="text-center">
-            <i className="fas fa-envelope"> tirypaul@gmail.com</i>
-            <i className="fas fa-phone-square"> +34 679 704 916</i>
-          </p>
+          <i className="fas fa-envelope"> tirypaul@gmail.com</i>
+          <i className="fas fa-phone-square"> +34 679 704 916</i>
         </div>
-        <div className="icons text-center mt-3">
+        <div className="icons">
           <i className="fab fa-linkedin fa-lg"></i>
           <i className="fab fa-facebook-square fa-lg"></i>
         </div>
